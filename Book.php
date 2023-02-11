83adebc29218161d1fb477b91b1c476b52714c01
baku67 <basile08@hotmail.fr>
2023-02-11 03:50:35 +0100

    class Book et test construct/affichage OK

--- Book.php
+++ Book.php
@@ -2,6 +2,74 @@
 
     class Book {
         
+        // Propriétés
+        private string $_title;
+        private int $_nbrOfPages;
+        private string $_publicationYear;
+        private float $_price;
+        private string $_currency;
+        private Author $_author;
+        // private AUTHOR
+
+        // Constructeur
+        public function __construct(string $title, int $nbrOfPages, string $publicationYear, float $price, string $currency, Author $author) {
+            $this->_title = $title;
+            $this->_nbrOfPages = $nbrOfPages;
+            $this->_publicationYear = $publicationYear;
+            $this->_price = $price;
+            $this->_currency = $currency;
+            $this->_author = $author;
+        }
+
+        // Accesseurs
+        public function getTitle(): string {
+            return $this->_title;
+        }
+        public function getNbrOfPages(): int {
+            return $this->_nbrOfPages;
+        }
+        public function getPublicationYear(): string {
+            return $this->_publicationYear;
+        }
+        public function getPrice(): float {
+            return $this->_price;
+        }
+        public function getCurrency(): string {
+            return $this->_currency;
+        }
+        public function getAuthor(): Author {
+            return $this->_author;
+        }
+        // Mutateurs
+        public function setTitle($newTitle) {
+            $this->_title = $newTitle;
+        }
+        public function setNbrOfPages($newNbrOfPages) {
+            $this->_nbrOfPages = $newNbrOfPages;
+        }
+        public function setDateOfPublication($newDateOfPublication) {
+            $this->_publicationYear = $newDateOfPublication;
+        }
+        public function setPrice($newPrice) {
+            $this->_price = $newPrice;
+        }
+        public function setCurrency($newCurrency) {
+            $this->_currency = $newCurrency;
+        }
+        public function setAuthor(Author $author) {
+            $this->_author = $author;
+        }
+
+        // Méthodes
+        public function printAuthor() {
+            return $this->getAuthor()->getFirstName() . " " . $this->getAuthor()->getLastName();
+        }
+
+
+        public function __toString() {
+            return "Livre: \"" . $this->getTitle() . "\"<br>" . $this->getNbrOfPages() . " pages<br>Publication: " . $this->getPublicationYear() . "<br>Prix: " . $this->getPrice() . " " . $this->getCurrency() . "<br>Auteur: " . $this->printAuthor();
+        }
+
     }
 
 
--- index.php
+++ index.php
@@ -4,7 +4,15 @@
     require "Author.php";
 
     $author1 = new Author("Guénolé", "De Montaigu");
+    $book1 = new Book("The Birth of Constantinople", 210, 330, 80, "Seashells", $author1);
+    $book2 = new Book("The Fall of Constantinople", 55, 1965, 125, "Gold", $author1);
+
+
+
 
     echo $author1;
+    echo "<br>";
+    echo $book1;
+
 
 ?>
